2f57ac515765e7b32c0a3dcce79d682eb460bef9
taiki <ttanaka@iis.u-tokyo.ac.jp>
2018-04-07 23:21:07 +0900

    Add image_detection part here

--- yolo.py
+++ yolo.py
@@ -176,3 +176,8 @@ def detect_img(yolo):
             r_image = yolo.detect_image(image)
             r_image.show()
     yolo.close_session()
+
+
+
+if __name__ == '__main__':
+    detect_img(YOLO())
