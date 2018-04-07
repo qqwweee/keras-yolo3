4d131ae2abbc3599fda2827ec54e6e3f0fe288ee
taiki <ttanaka@iis.u-tokyo.ac.jp>
2018-04-07 22:13:30 +0900

    Add Window mode

--- yolo.py
+++ yolo.py
@@ -157,7 +157,7 @@ def detect_video(yolo,video_path):
             curr_fps = 0
         cv2.putText(result, text=fps, org=(3, 15), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.50,
                     color=(255, 0, 0), thickness=2)
-
+        cv2.namedWindow("result", cv2.WINDOW_NORMAL)
         cv2.imshow("result",result)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
