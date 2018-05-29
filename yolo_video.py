ad3553ce052b2e83c7e076f74e7110cabff2b8d0
b02902131 <b02902131@ntu.edu.tw>
2018-05-29 17:34:31 +0800

    modify yolo_video to feed video path

--- yolo_video.py
+++ yolo_video.py
@@ -2,8 +2,13 @@
 from yolo import YOLO
 from yolo import detect_video
 
-
+import sys
 
 if __name__ == '__main__':
-    video_path='path2your-video'
+
+    if len(sys.argv) < 2:
+        print("Usage: $ python {0} [video_path]", sys.argv[0])
+        exit()
+
+    video_path = sys.argv[1]
     detect_video(YOLO(), video_path)
