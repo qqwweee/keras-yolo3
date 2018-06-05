5b08b0313a499217bd268de450ccec5368fbd8b6
Bigyo.Dayo <b02902131@csie.ntu.edu.tw>
2018-06-05 16:21:13 +0800

    Some modifications which let user use it easilier (#87)
    * modify yolo_video to feed video path
    
    * check parameter before import something
    
    * implement video output

--- yolo_video.py
+++ yolo_video.py
@@ -1,9 +1,14 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: $ python {0} [video_path, [, output_path]]", sys.argv[0])
+    exit()
 
 from yolo import YOLO
 from yolo import detect_video
 
-
-
 if __name__ == '__main__':
-    video_path='path2your-video'
-    detect_video(YOLO(), video_path)
+    video_path = sys.argv[1]
+    if len(sys.argv) > 2:
+        output_path = sys.argv[2]
+    detect_video(YOLO(), video_path, output_path)
