83f787c4425caff7349816886e28eeaf80a1b659
b02902131 <b02902131@ntu.edu.tw>
2018-06-05 12:28:43 +0800

    check parameter before import something

--- yolo_video.py
+++ yolo_video.py
@@ -1,14 +1,12 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: $ python {0} [video_path, [, output_path]]", sys.argv[0])
+    exit()
 
 from yolo import YOLO
 from yolo import detect_video
 
-import sys
-
 if __name__ == '__main__':
-
-    if len(sys.argv) < 2:
-        print("Usage: $ python {0} [video_path]", sys.argv[0])
-        exit()
-
     video_path = sys.argv[1]
     detect_video(YOLO(), video_path)
