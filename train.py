3bff8467b9ad92eb4237a1c2ff1c984ffd67284d
julia0 <jmoseyko@gmail.com>
2018-07-12 16:26:38 -0700

    resolve merge issues

--- train.py
+++ train.py
@@ -17,7 +17,6 @@ def _main():
     annotation_path = 'PATH_TO_ANNOTATIONS'
     log_dir = 'logs/000/'
     classes_path = 'PATH_TO_CLASSES' # example in model/coco_classes.txt
-    classes_path = 'PATH_TO_CLASSES'
     anchors_path = 'model_data/yolo_anchors.txt'
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
