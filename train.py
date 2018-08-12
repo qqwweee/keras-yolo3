e89c8fc4783d26d2ac58da1e7848da14c91cc945
JK Jung <jkjung13@gmail.com>
2018-08-13 00:04:47 +0800

    Use darknet53 as pre-trained weights; adjust batch size

--- train.py
+++ train.py
@@ -13,6 +13,11 @@ from yolo3.model import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_l
 from yolo3.utils import get_random_data
 
 
+USE_DARKNET53 = True
+BATCH_SIZE_1 = 32
+BATCH_SIZE_2 = 6
+
+
 def _main():
     annotation_path = 'train.txt'
     log_dir = 'logs/000/'
@@ -24,13 +29,21 @@ def _main():
 
     input_shape = (416,416) # multiple of 32, hw
 
-    is_tiny_version = len(anchors)==6 # default setting
-    if is_tiny_version:
-        model = create_tiny_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
-    else:
+    #is_tiny_version = len(anchors)==6 # default setting
+    #if is_tiny_version:
+    #    model = create_tiny_model(input_shape, anchors, num_classes,
+    #        freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
+    #else:
+    #    model = create_model(input_shape, anchors, num_classes,
+    #        freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
+
+    if USE_DARKNET53:
         model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
+            freeze_body=2, weights_path='model_data/darknet53_weights.h5')
+    else:
+        # otherwise use the default yolov3 weights
+        model = create_model(input_shape, anchors, num_classes,
+            freeze_body=2, weights_path='model_data/yolo_weights.h5')
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
@@ -54,7 +67,7 @@ def _main():
             # use custom yolo_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred})
 
-        batch_size = 32
+        batch_size = BATCH_SIZE_1
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
                 steps_per_epoch=max(1, num_train//batch_size),
@@ -73,7 +86,7 @@ def _main():
         model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         print('Unfreeze all of the layers.')
 
-        batch_size = 32 # note that more GPU memory is required after unfreezing the body
+        batch_size = BATCH_SIZE_2 # note that more GPU memory is required after unfreezing the body
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
             steps_per_epoch=max(1, num_train//batch_size),
