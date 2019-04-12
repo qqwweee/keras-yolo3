e0df4e371d15cf74ca2de670eb7954fee4a55f13
Sean Seung Won Lee <swdev1202@github.com>
2019-04-12 06:05:09 +0000

    30 epochs, yolo default h5 back

--- yolo.py
+++ yolo.py
@@ -20,9 +20,9 @@ from keras.utils import multi_gpu_model
 
 class YOLO(object):
     _defaults = {
-        "model_path": 'model_data/temp.h5',
+        "model_path": 'model_data/yolo.h5',
         "anchors_path": 'model_data/yolo_anchors.txt',
-        "classes_path": 'model_data/crowd_ai_classes.txt',
+        "classes_path": 'model_data/voc_classes.txt',
         "score" : 0.3,
         "iou" : 0.45,
         "model_image_size" : (416, 416),
