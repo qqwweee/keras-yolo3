df0ea7441f77660020fbbac56065656198a4b63a
amit cahanovich <amitca71@gmail.com>
2022-11-04 13:53:08 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -14,8 +14,8 @@ import numpy as np
 from keras import backend as K
 from keras.layers import (Conv2D, Input, ZeroPadding2D, Add,
                           UpSampling2D, MaxPooling2D, Concatenate)
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.normalization import BatchNormalization
+from keras.layers import LeakyReLU
+from keras.layers import BatchNormalization
 from keras.models import Model
 from keras.regularizers import l2
 from keras.utils.vis_utils import plot_model as plot
